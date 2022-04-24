c99ee3170668fb372a20c8a9673fcc0f30e4613a
Alexandru Gherman <alexandru.gherman@boots.co.uk>
2022-04-24 09:39:02 +0300

    Played with states between react components

--- 01-react-app/src/tic-tac-toe/Board.jsx
+++ 01-react-app/src/tic-tac-toe/Board.jsx
@@ -2,29 +2,39 @@ import React from 'react';
 import Square from './Square';
 
 class Board extends React.Component {
-  // TODO add array of 9 nulls for constructor for state and to find
-  //the winner
   constructor(props) {
     super(props);
-    this.state = { playerState: 'X' };
+    this.state = {
+      squares: Array(9).fill(null),
+      playerState: 'X',
+      winner: false,
+    };
+    this.arraySquare = new Array(9).fill(null);
   }
 
-  handleCallback = () => {
-    this.setState({ playerState: this.state.playerState == 'X' ? 'O' : 'X' });
-  };
+  //TODO understand this approch, try using arrow function
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({ squares: squares });
+    // this.state.squares[i] = 'X';
+  }
+  //   this.setState({ playerState: this.state.playerState == 'X' ? 'O' : 'X' });
 
   renderSquare(i) {
     return (
       <Square
-        value={i}
-        playerState={this.state.playerState}
-        parentCallback={this.handleCallback}
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
       />
     );
   }
 
   render() {
-    const status = `Next player: ${this.state.playerState}`;
+    const status = !this.checkWinner()
+      ? `Next player: ${this.state.playerState}`
+      : `Winner: ${this.state.playerState == 'X' ? 'O' : 'X'}`;
+
     return (
       <div className="board">
         <div className="status">{status}</div>
@@ -46,6 +56,104 @@ class Board extends React.Component {
       </div>
     );
   }
+
+  checkWinner = () => {
+    if (!this.state.winner) {
+      if (
+        (this.arraySquare[0] == 'X' &&
+          this.arraySquare[1] == 'X' &&
+          this.arraySquare[2] == 'X') ||
+        (this.arraySquare[0] == 'O' &&
+          this.arraySquare[1] == 'O' &&
+          this.arraySquare[2] == 'O')
+      ) {
+        this.state.winner = true;
+      }
+      if (
+        (this.arraySquare[3] == 'X' &&
+          this.arraySquare[4] == 'X' &&
+          this.arraySquare[5] == 'X') ||
+        (this.arraySquare[3] == 'O' &&
+          this.arraySquare[4] == 'O' &&
+          this.arraySquare[5] == 'O')
+      ) {
+        this.state.winner = true;
+      }
+      if (
+        (this.arraySquare[6] == 'X' &&
+          this.arraySquare[7] == 'X' &&
+          this.arraySquare[8] == 'X') ||
+        (this.arraySquare[6] == 'O' &&
+          this.arraySquare[7] == 'O' &&
+          this.arraySquare[8] == 'O')
+      ) {
+        this.state.winner = true;
+      }
+      if (
+        (this.arraySquare[0] == 'X' &&
+          this.arraySquare[3] == 'X' &&
+          this.arraySquare[6] == 'X') ||
+        (this.arraySquare[0] == 'O' &&
+          this.arraySquare[3] == 'O' &&
+          this.arraySquare[6] == 'O')
+      ) {
+        this.state.winner = true;
+      }
+      if (
+        (this.arraySquare[1] == 'X' &&
+          this.arraySquare[4] == 'X' &&
+          this.arraySquare[7] == 'X') ||
+        (this.arraySquare[1] == 'O' &&
+          this.arraySquare[4] == 'O' &&
+          this.arraySquare[7] == 'O')
+      ) {
+        this.state.winner = true;
+      }
+      if (
+        (this.arraySquare[2] == 'X' &&
+          this.arraySquare[5] == 'X' &&
+          this.arraySquare[8] == 'X') ||
+        (this.arraySquare[2] == 'O' &&
+          this.arraySquare[5] == 'O' &&
+          this.arraySquare[8] == 'O')
+      ) {
+        this.state.winner = true;
+      }
+      if (
+        (this.arraySquare[0] == 'X' &&
+          this.arraySquare[4] == 'X' &&
+          this.arraySquare[8] == 'X') ||
+        (this.arraySquare[0] == 'O' &&
+          this.arraySquare[4] == 'O' &&
+          this.arraySquare[8] == 'O')
+      ) {
+        this.state.winner = true;
+      }
+      if (
+        (this.arraySquare[2] == 'X' &&
+          this.arraySquare[4] == 'X' &&
+          this.arraySquare[6] == 'X') ||
+        (this.arraySquare[2] == 'O' &&
+          this.arraySquare[4] == 'O' &&
+          this.arraySquare[6] == 'O')
+      ) {
+        this.state.winner = true;
+      }
+    }
+    return this.state.winner;
+  };
+  // handleCallback = () => {
+  //   this.setState({ playerState: this.state.playerState == 'X' ? 'O' : 'X' });
+  // };
+
+  // onClick = i => {
+  //   if (!this.arraySquare[i] && !this.winnerState) {
+  //     this.arraySquare[i] = this.playerState;
+  //   }
+  //   this.setState({ playerState: this.state.playerState == 'X' ? 'O' : 'X' });
+
+  //   return this.arraySquare[i];
+  // };
 }
 
 export default Board;
--- 01-react-app/src/tic-tac-toe/Square.jsx
+++ 01-react-app/src/tic-tac-toe/Square.jsx
@@ -3,26 +3,40 @@ import React from 'react';
 class Square extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { player: null };
   }
 
   render() {
     return (
       <div>
-        <button
-          onClick={() => {
-            if (this.state.player == null) {
-              this.setState({ player: this.props.playerState });
-              this.props.parentCallback();
-            }
-          }}
-          className="square"
-        >
-          {this.state.player}
+        <button className="square" onClick={this.props.onClick}>
+          {this.props.value}
         </button>
       </div>
     );
   }
 }
+
+// render() {
+//   return (
+//     <div>
+//       <button
+//         onClick={() => {
+//           if (
+//             !this.props.arraySquare[this.props.value] &&
+//             this.props.winnerState == false
+//           ) {
+//             this.props.arraySquare[this.props.value] = this.props.playerState;
+//             // this.setState({ player: this.props.playerState });
+//             this.props.parentCallback();
+//           }
+//         }}
+//         className="square"
+//       >
+//         {this.props.arraySquare[this.props.value]}
+//       </button>
+//     </div>
+//   );
+// }
+//}
 
 export default Square;
